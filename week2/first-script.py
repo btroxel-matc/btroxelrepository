c22126d09fa97f56c68eec27fbc40fb21c2dd6c1
btroxel-matc <btroxel@madisoncollege.edu>
2022-09-08 14:14:20 -0500

    READY FOR GRADING - Adding log files for assignment submission

---
+++ week2/first-script.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Author: Bryan Troxel
+# Description: This is my first python script.
+my_full_name = "Bryan Troxel"
+
+print(my_full_name)
+print("The use of variables will make the process of writing scripts less time consuming.")
