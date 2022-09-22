1ce556910dd4059c49d9c1a91b23dbc1e9297638
btroxel-matc <btroxel@madisoncollege.edu>
2022-09-22 17:28:38 -0500

    sequences.py

---
+++ week4/sequences.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+varString = "Here is a nice string to slice"
+varList = ["Here","is","a","nice","list","to","slice"]
+print(varString[3::1])
+print(varString[0:3:1])
+print(varString[3:12:1])
+print(varString[0::2])
+print(varString[::-1])
+print(varList[::-1])
+print(varList[-5:-8:-1])
+print(varList[2:4:1])
+print(varList[::3])
+print(varList[1::1])
+
+for element in varString:
+    print(element)
+
+for element in varList:
+    print(element)
