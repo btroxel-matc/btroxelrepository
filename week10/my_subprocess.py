c215d0837a4c7b90af7f72f04d48095841d72c6b
btroxel-matc <btroxel@madisoncollege.edu>
2022-11-03 14:05:56 -0500

    my_subprocess.py

---
+++ week10/my_subprocess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author: Bryan Troxel
+#Description: This lab covers Subprocesses.
+
+import subprocess
+
+myProc=subprocess.run(['ps','-axco','command'],stdout=subprocess.PIPE)
+myProcString=myProc.stdout.decode()
+myProcList=myProcString.split('\n')
+
+for line in myProcList:
+    print(line)
+
+print(f"{len(myProcList[2::])}")
